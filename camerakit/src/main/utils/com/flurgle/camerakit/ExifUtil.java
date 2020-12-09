a8b9e4c6472b70586eae169ffbf7b8a03678171d
Attila Hegedüs <attilahegedus90@gmail.com>
2020-12-09 14:48:02 +0100

    Expose functions from ExifUtil

--- camerakit/src/main/utils/com/flurgle/camerakit/ExifUtil.java
+++ camerakit/src/main/utils/com/flurgle/camerakit/ExifUtil.java
@@ -29,18 +29,17 @@ public class ExifUtil {
 
     public static Bitmap decodeBitmapWithRotation(byte[] picture, boolean frontFacing) {
         Bitmap bitmap = BitmapFactory.decodeByteArray(picture, 0, picture.length);
-        Matrix matrix = getBitmapRotation(picture, frontFacing);
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-    }
-
-    public static Matrix getBitmapRotation(byte[] picture, boolean frontFacing) {
         int orientation = ExifInterface.ORIENTATION_UNDEFINED;
         try {
             orientation = getExifOrientation(new ByteArrayInputStream(picture));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Matrix matrix = getBitmapRotation(orientation, frontFacing);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
 
+    public static Matrix getBitmapRotation(int orientation, boolean frontFacing) {
         Matrix matrix = new Matrix();
 
         switch (orientation) {
@@ -80,7 +79,7 @@ public class ExifUtil {
         return matrix;
     }
 
-    private static int getExifOrientation(InputStream inputStream) throws IOException {
+    public static int getExifOrientation(InputStream inputStream) throws IOException {
         ExifInterface exif = new ExifInterface(inputStream);
         return exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
     }
